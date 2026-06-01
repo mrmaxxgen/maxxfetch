9112c7660d69faa9c2291e7587e884540284caf4
mrmaxxarch <kevindetogni03@gmail.com>
2026-06-01 12:23:30 +0200

    modificato:             maxxfetch.cpp

--- maxxfetch.cpp
+++ maxxfetch.cpp
@@ -2,11 +2,50 @@
 
 using namespace std;
 
+const char *user = R"(
+echo user: $USER
+)";
+
+const char *hostname = R"(
+echo hostname: $HOSTNAME
+)";
+
+const char *distro = R"(
+echo distro: $(cat /etc/os-release | grep PRETTY_NAME | cut -d'=' -f2)
+)";
+
+const char *kernel = R"(
+echo kernel: $(uname -r)
+)";
+
+const char *uptime = R"(
+echo uptime: $(cat /proc/uptime | cut -d' ' -f1)
+)";
+
+const char *shell = R"(
+echo shell: $SHELL
+)";
+
+const char *terminal = R"(
+echo terminal: $TERM
+)";
+
+const char *cpu = R"(
+echo CPU: $(uname -pm)
+)";
+
+const char *memory = R"(
+echo MEM: $(cat /proc/meminfo | grep Active | cut -d':' -f2) / $(cat /proc/meminfo | grep MemAvailable | cut -d':' -f2)
+)";
+
 int main() {
-	printf("user: \n");
-	system("echo $USER");
-	printf("hostname: \n");
-	system("echo $HOSTNAME");
-	printf("distro: \n");
-	system("cat /etc/os-release | grep PRETTY_NAME | cut -d'=' -f2");
+	system(user);
+	system(hostname);
+	system(distro);
+	system(kernel);
+	system(uptime);
+	system(shell);
+	system(terminal);
+	system(cpu);
+	system(memory);
 }
